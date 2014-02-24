93da0f908bf79eab7476e59b428a39d03387b1b5
rcolbert <rob.isConnected@gmail.com>
2014-02-24 15:23:52 -0500

    Config swap (again, I suck at this, always in too big of a hurry and miss it)

--- frontend/app/scripts/services/configuration.js
+++ frontend/app/scripts/services/configuration.js
@@ -10,8 +10,8 @@ angular.module('robcolbertApp')
 
     var apiServer = {
       'scheme': 'http://',
-      'host': 'localhost',
-      //'host': 'robcolbert.com',
+      //'host': 'localhost',
+      'host': 'robcolbert.com',
       'port': 10010
     };
 
