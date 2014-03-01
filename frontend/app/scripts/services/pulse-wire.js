a91331bf7f20472c01e3f49fe33338d024d656ec
rcolbert <rob.isConnected@gmail.com>
2014-02-28 21:00:27 -0500

    Disabled the PulseWire service. Server's getting hit hard enough.

--- frontend/app/scripts/services/pulse-wire.js
+++ frontend/app/scripts/services/pulse-wire.js
@@ -13,51 +13,53 @@ function PulseWire($rootScope, Configuration, UserSession) {
 }
 
 PulseWire.prototype.connect = function ( ) {
-  var connectUrl = this.Configuration.buildApiUrl('/');
-  //connectUrl = 'http://localhost:10010/';
-  console.log('socket.io connecting to', connectUrl);
-  this.socket = io.connect(connectUrl);
-  this.attach();
+  return;
+//   var connectUrl = this.Configuration.buildApiUrl('/');
+//   //connectUrl = 'http://localhost:10010/';
+//   console.log('socket.io connecting to', connectUrl);
+//   this.socket = io.connect(connectUrl);
+//   this.attach();
 };
 
 PulseWire.prototype.emit = function (eventName, eventData) {
-  this.socket.emit(eventName, eventData);
+//   this.socket.emit(eventName, eventData);
 };
 
 PulseWire.prototype.attach = function ( ) {
-  var self = this;
-
-  this.socket.on('hello', function (data) {
-    console.log('pulsewire.hello', data);
-    self.server = data;
-    self.open = true;
-    self.$rootScope.$broadcast('pulsewire.hello', data);
-  });
-
-  this.socket.on('post', function (data) {
-    console.log('pulsewire.post', data);
-    self.$rootScope.$broadcast('pulsewire.post', data);
-  });
-
-  this.socket.on('thought', function (data) {
-    console.log('pulsewire.thought', data);
-    self.$rootScope.$broadcast('pulsewire.thought', data);
-  });
-
-  this.socket.on('comment', function (data) {
-    console.log('pulsewire.comment', data);
-    self.$rootScope.$broadcast('pulsewire.comment', data);
-  });
-
-  this.socket.on('mail', function (data) {
-    console.log('pulsewire.mail', data);
-    self.$rootScope.$broadcast('pulsewire.mail', data);
-  });
-
-  this.socket.on('message', function (data) {
-    console.log('pulsewire.message', data);
-    self.$rootScope.$broadcast('pulsewire.message', data);
-  });
+  return;
+//   var self = this;
+//
+//   this.socket.on('hello', function (data) {
+//     console.log('pulsewire.hello', data);
+//     self.server = data;
+//     self.open = true;
+//     self.$rootScope.$broadcast('pulsewire.hello', data);
+//   });
+//
+//   this.socket.on('post', function (data) {
+//     console.log('pulsewire.post', data);
+//     self.$rootScope.$broadcast('pulsewire.post', data);
+//   });
+//
+//   this.socket.on('thought', function (data) {
+//     console.log('pulsewire.thought', data);
+//     self.$rootScope.$broadcast('pulsewire.thought', data);
+//   });
+//
+//   this.socket.on('comment', function (data) {
+//     console.log('pulsewire.comment', data);
+//     self.$rootScope.$broadcast('pulsewire.comment', data);
+//   });
+//
+//   this.socket.on('mail', function (data) {
+//     console.log('pulsewire.mail', data);
+//     self.$rootScope.$broadcast('pulsewire.mail', data);
+//   });
+//
+//   this.socket.on('message', function (data) {
+//     console.log('pulsewire.message', data);
+//     self.$rootScope.$broadcast('pulsewire.message', data);
+//   });
 };
 
 PulseWire.$inject = [
