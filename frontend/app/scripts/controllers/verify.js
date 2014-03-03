40481d7b4aea6a953ecffb7b09d6ab5c1eac5700
rcolbert <rob.isConnected@gmail.com>
2014-03-03 05:18:05 -0500

    User account creation and email verification completed

---
+++ frontend/app/scripts/controllers/verify.js
@@ -0,0 +1,81 @@
+// controllers/verify.js
+// Copyright (C) 2014 Rob Colbert <rob.isConnected@gmail.com>
+
+'use strict';
+
+function VerifyCtrl ($scope, $route, $interval, Users) {
+  var intervalPromise = null;
+
+  $scope.nodeIsDone = false;
+  $scope.showLoginButton = false;
+
+  Users.verify(
+    {
+      'userId': $route.current.params.u,
+      'k': $route.current.params.k
+    },
+    null,
+    function onVerifySuccess (response) {
+      console.log('verify success', response);
+      $scope.nodeIsDone = true;
+      $scope.showLoginButton = true;
+      $scope.nodeStatusMessage = 'Damn, homie. You actually check out. Welcome aboard!';
+    },
+    function onVerifyFailed (error) {
+      console.log('verify failed', error);
+      $scope.nodeIsDone = true;
+      $scope.percentComplete = 100;
+      $scope.statusMessage = error.data.msg;
+      $scope.nodeStatusMessage = error.data.msg;
+      if (intervalPromise !== null) {
+        $interval.cancel(intervalPromise);
+        intervalPromise = null;
+      }
+    }
+  );
+
+  // This is some humorous shit I wrote to poke fun at how this
+  // tends to look on sites built with Java.
+  $scope.percentComplete = 0;
+  $scope.statusMessage = 'Contacting Pulsar API server...';
+  intervalPromise = $interval(function ( ) {
+    if ($scope.percentComplete >= 100) {
+      if (intervalPromise !== null) {
+        $interval.cancel(intervalPromise);
+        intervalPromise = null;
+      }
+      return;
+    }
+    $scope.percentComplete += 1;
+    if ($scope.percentComplete > 20 && $scope.percentComplete <= 33) {
+      $scope.statusMessage = 'Submitting authentication token...';
+    }
+    if ($scope.percentComplete > 33 && $scope.percentComplete <= 60) {
+      $scope.statusMessage = 'The ORACLE wants you to know: There is no spoon...';
+    }
+    if ($scope.percentComplete > 60 && $scope.percentComplete <= 80) {
+      $scope.statusMessage = 'Shit\'s actually getting real all up in this piece right about now...';
+    }
+    if ($scope.percentComplete > 80 && $scope.percentComplete <= 95) {
+      $scope.statusMessage = 'Shit\'s actually getting real all up in this piece right about now...very real...';
+    }
+    if ($scope.percentComplete === 100) {
+      $scope.statusMessage = 'Damn, homie. You actually check out. Welcome aboard!';
+    }
+  }, 100);
+
+  $scope.showLoginModal = function ( ) {
+    $('#userLoginModal').modal('show');
+  };
+
+}
+
+VerifyCtrl.$inject = [
+  '$scope',
+  '$route',
+  '$interval',
+  'Users'
+];
+
+angular.module('robcolbertApp')
+.controller('VerifyCtrl', VerifyCtrl);
---
+++ frontend/app/scripts/services/users.js
@@ -0,0 +1,26 @@
+// services/users.js
+// Copyright (C) 2014 Rob Colbert <rob.isConnected@gmail.com>
+
+'use strict';
+
+function UsersService ($resource, Configuration) {
+  var serviceUrl = Configuration.buildApiUrl('/users/:userId');
+  var verifyUrl = Configuration.buildApiUrl('/users/:userId/verify');
+  var defaultParameters = null;
+  return $resource(serviceUrl, defaultParameters, {
+    'create': { 'method': 'POST' },
+    'verify': { 'method': 'POST', 'url': verifyUrl },
+    'get': { 'method': 'GET' },
+    'update': { 'method': 'PUT' },
+    'delete': { 'method': 'DELETE' },
+    'list': { 'method': 'GET', 'isArray': true }
+  });
+}
+
+PostsService.$inject = [
+  '$resource',
+  'Configuration'
+];
+
+angular.module('robcolbertApp')
+.service('Users', UsersService);
